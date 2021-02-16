0da4d67689f9698a7d909371bec55f0b0de57d98
unknown <shireenkk28@gmail.com>
2021-02-17 00:09:14 +0530

    added flipcoin.sh

--- flipcoin.sh
+++ flipcoin.sh
@@ -1,10 +1,10 @@
 
 #! /bin/bash -x
 
-i=0
 H=0
 T=0
-while [ $H -lt 21 -a $T -lt 21 ]
+diff=0
+while [ $H -lt 21 -a $T -lt 21 -o $diff -lt 2 -a $diff -gt -2 ]
 do
 	coin=$(( RANDOM%2 ))
 	if [ $coin -eq 1 ]
@@ -13,9 +13,17 @@ do
 	else
 		T=$(( T+1 ))
 	fi
+	diff=$(( H-T ))
 done
 echo "heads" $H
 echo "tails" $T
+echo
 echo "heads won by" $H "times"
 echo "tails won by" $T "times"
+if [ $H -gt $T ]
+then
+	echo "heads won" $diff "times"
+else
+	echo "tails won" $(( -1*diff )) "times"
+fi
 
